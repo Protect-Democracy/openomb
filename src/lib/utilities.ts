87ad037abee9f52c4d0f0404f65848723dd4ae24
Alan Palazzolo <190733+zzolo@users.noreply.github.com>
2024-04-15 15:49:42 -0400

    Adding cache control headers.

--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -2,6 +2,8 @@
  * General utilities that should run fine on server or client.
  */
 
+import { toDate, toZonedTime } from 'date-fns-tz';
+
 /**
  * Determine if production using NODE_ENV.
  *
@@ -10,3 +12,42 @@
  * @returns True if in production
  */
 export const isProduction = (): boolean => import.meta.env.PROD;
+
+/**
+ * Get seconds to the next specific time in a timezone.
+ *
+ * For example, how many seconds from 11:30pm to 12:30pm (1hr)
+ * or from 5:20pm to 3:40am (9hrs 20minutes)
+ *
+ * @param hour Hour in 24 format of the day
+ * @param minute Minute of the hour
+ * @param timezone Timezone to convert to
+ */
+export const secondsToZonedTime = function (
+  hour: number,
+  minute: number,
+  timezone: string,
+  dateToCompare: string | Date = new Date(),
+  minimumSeconds: number = 30
+): number {
+  // We want to get the date time in the timezone.  Unsure why this is difficult
+  // in date-fns, but this seems to work.
+  const parsedDateToCompare = toDate(dateToCompare, { timeZone: timezone });
+  const zonedDateToCompare = toZonedTime(parsedDateToCompare, timezone);
+  const currentHour = zonedDateToCompare.getHours();
+  const currentMinute = zonedDateToCompare.getMinutes();
+
+  // Convert to seconds
+  const currentSeconds = currentHour * 60 * 60 + currentMinute * 60;
+  const targetSeconds = hour * 60 * 60 + minute * 60;
+
+  let diffSeconds = 0;
+  if (currentSeconds > targetSeconds) {
+    diffSeconds = 24 * 60 * 60 - currentSeconds + targetSeconds;
+  }
+  else {
+    diffSeconds = targetSeconds - currentSeconds;
+  }
+
+  return Math.max(diffSeconds, minimumSeconds);
+};
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,17 +1,26 @@
+import { secondsToZonedTime, isProduction } from '$lib/utilities.js';
+
 /** @type {import('./$types').PageLoad} */
 export async function load({ setHeaders }) {
+  const secondsForCache = secondsToZonedTime(5, 0, 'America/New_York');
+  const revalidateSeconds = 60 * 60;
+
   // Common headers.
   //
   // Note: These only apply to code routes and not assets or static files.
-  // Though it does look like the default headers for assets are reasonable.
-  // On the other hand, static files don't look to have any cache headers
-  // which is not great.
+  // Though it does look like the default headers for assets are reasonable
+  // and they use hashes.  On the other hand, static files don't look to
+  // have any cache headers which is not great.
   //
   // Note: These specific headers cannot be set again otherwise an error
   // will be thrown.
   //
   // Note: CSP headers are configured in `svelte.config.js`
   setHeaders({
+    // Cache headers
+    'Cache-Control': `public, max-age=${isProduction() ? secondsForCache : 1}, stale-while-revalidate=${revalidateSeconds}`,
+
+    // Security headers
     // TODO: Add report-uri to Sentry
     // See: https://docs.sentry.io/product/security-policy-reporting/#expect-ct
     'Expect-CT': 'enforce, max-age=3600',
