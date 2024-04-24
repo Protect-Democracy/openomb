96a4390d1b8235fbe25aaedbcc5c13100a919be5
nkt <nicholas.tulach@voteshield.us>
2024-04-24 10:28:27 -0400

    Update infrastructure

--- tofu/ecs.tf
+++ tofu/ecs.tf
@@ -10,12 +10,16 @@ resource "aws_ecs_service" "apportionments_app" {
   desired_count        = 1
   force_new_deployment = true
 
+  triggers = {
+    redeployment = plantimestamp()
+  }
+
   network_configuration {
     assign_public_ip = false
 
     security_groups = [
       aws_security_group.egress_all.id,
-      aws_security_group.ingress_api.id,
+      aws_security_group.ingress_app.id,
     ]
 
     subnets = [
@@ -85,3 +89,102 @@ resource "aws_ecs_task_definition" "apportionments_app" {
   network_mode = "awsvpc"
 }
 
+resource "aws_ecs_task_definition" "apportionments_collect" {
+  family = "apportionments-collect"
+
+  container_definitions = jsonencode([
+    {
+      "name" : "apportionments-collect",
+      "image" : "${aws_ecr_repository.ecr.repository_url}:latest"
+      "essential" : true,
+      "environment" : [
+        {
+          "name" : "APPORTIONMENTS_DB_HOST",
+          "value" : "${aws_rds_cluster.apportionments.endpoint}"
+        },
+        {
+          "name" : "APPORTIONMENTS_DB_PORT",
+          "value" : "${tostring(aws_rds_cluster.apportionments.port)}"
+        },
+        {
+          "name" : "APPORTIONMENTS_DB_NAME",
+          "value" : "${aws_rds_cluster.apportionments.database_name}"
+        }
+      ],
+      "secrets" : [
+        {
+          "name" : "APPORTIONMENTS_DB_AUTHENTICATION",
+          "valueFrom" : "${aws_rds_cluster.apportionments.master_user_secret[0].secret_arn}"
+        }
+      ],
+      "command" : ["build-collect/collect.js"],
+      "logConfiguration" : {
+        "logDriver" : "awslogs",
+        "options" : {
+          "awslogs-region" : "${var.region}",
+          "awslogs-group" : "/ecs/apportionments-collect",
+          "awslogs-stream-prefix" : "ecs"
+        }
+      }
+    }
+  ])
+
+  execution_role_arn = aws_iam_role.apportionments_app_task_execution_role.arn
+
+  # Minimum values for Fargate
+  cpu                      = 256
+  memory                   = 512
+  requires_compatibilities = ["FARGATE"]
+
+  network_mode = "awsvpc"
+}
+
+resource "aws_ecs_task_definition" "apportionments_migrate" {
+  family = "apportionments-migrate"
+
+  container_definitions = jsonencode([
+    {
+      "name" : "apportionments-migrate",
+      "image" : "${aws_ecr_repository.ecr.repository_url}:latest"
+      "essential" : true,
+      "environment" : [
+        {
+          "name" : "APPORTIONMENTS_DB_HOST",
+          "value" : "${aws_rds_cluster.apportionments.endpoint}"
+        },
+        {
+          "name" : "APPORTIONMENTS_DB_PORT",
+          "value" : "${tostring(aws_rds_cluster.apportionments.port)}"
+        },
+        {
+          "name" : "APPORTIONMENTS_DB_NAME",
+          "value" : "${aws_rds_cluster.apportionments.database_name}"
+        }
+      ],
+      "secrets" : [
+        {
+          "name" : "APPORTIONMENTS_DB_AUTHENTICATION",
+          "valueFrom" : "${aws_rds_cluster.apportionments.master_user_secret[0].secret_arn}"
+        }
+      ],
+      "command" : ["build-migrate/migrate.js"],
+      "logConfiguration" : {
+        "logDriver" : "awslogs",
+        "options" : {
+          "awslogs-region" : "${var.region}",
+          "awslogs-group" : "/ecs/apportionments-migrate",
+          "awslogs-stream-prefix" : "ecs"
+        }
+      }
+    }
+  ])
+
+  execution_role_arn = aws_iam_role.apportionments_app_task_execution_role.arn
+
+  # Minimum values for Fargate
+  cpu                      = 256
+  memory                   = 512
+  requires_compatibilities = ["FARGATE"]
+
+  network_mode = "awsvpc"
+}
---
+++ tofu/outputs.tf
@@ -0,0 +1,23 @@
+output "alb_url" {
+  value = "http://${aws_alb.apportionments_app.dns_name}"
+}
+
+output "ecs_cluster_arn" {
+  value = aws_ecs_cluster.apportionments_app.arn
+}
+
+output "app_task_definition_arn" {
+  value = aws_ecs_task_definition.apportionments_app.arn
+}
+
+output "collect_task_definition_arn" {
+  value = aws_ecs_task_definition.apportionments_collect.arn
+}
+
+output "migrate_task_definition_arn" {
+  value = aws_ecs_task_definition.apportionments_migrate.arn
+}
+
+output "run_task_network_configuration_string" {
+  value = "awsvpcConfiguration={subnets=[${aws_subnet.private_a.id},${aws_subnet.private_b.id}],securityGroups=[${aws_security_group.egress_all.id},${aws_security_group.database.id}],assignPublicIp=DISABLED}"
+}
