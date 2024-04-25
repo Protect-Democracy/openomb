76ed1f63c011202e4fd73ca3bd592b52b62bb328
Alan Palazzolo <190733+zzolo@users.noreply.github.com>
2024-04-25 11:53:49 -0400

    Adding default error pages.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { page } from '$app/stores';
+  throw new Error('help');
+</script>
+
+<div class="page-container content-container">
+  <!-- eslint-disable svelte/valid-compile -->
+  <h1>Error: {$page.status}</h1>
+
+  <p>{$page.error?.message || 'Unknown error'}. <a href="/">Go back to home page</a>.</p>
+  <!-- eslint-enable svelte/valid-compile -->
+</div>
