fd96c8095112939b5ae9047b2d8da8c297552879
Alan Palazzolo <190733+zzolo@users.noreply.github.com>
2024-06-24 17:17:42 -0400

    Minor updates to no results styles.

--- src/routes/search/NoResults.svelte
+++ src/routes/search/NoResults.svelte
@@ -3,47 +3,42 @@
 
   // Props
   export let resultType: 'file' | 'account';
-
-  const resultText = resultType == 'file' ? 'a file' : 'an account';
 </script>
 
 <svelte.fragment>
-  <p>No {resultType}s were found from your criteria. Please refine your search and try again.</p>
+  <p>
+    <strong>No {resultType}s were found</strong> from your criteria. Please refine your search and try
+    again. Need some tips?
+  </p>
 
-  <dl>
-    <dh>Need help finding {resultText}?</dh>
-    <dd>
-      Ensure separate keyword values are separated by commas (e.g. <i>navy,department of defense</i
+  <ul>
+    <li>
+      Ensure separate keyword values are separated by commas (e.g. <em
+        >navy, department of defense</em
       >).
-    </dd>
+    </li>
+
     {#if resultType == 'file'}
-      <dd>
+      <li>
         Try a keyword search on the <a
           target="_blank"
           rel="noopener noreferrer"
           href="https://www.archives.gov/federal-register/laws/current.html">Public Law Number</a
         >.
-      </dd>
-      <dd>
+      </li>
+
+      <li>
         Ensure that all keywords are expected to be found within the file (search does not return
         partial matches).
-      </dd>
-      <dd>
+      </li>
+
+      <li>
         Read our <a target="_blank" href="/faq#faq-how-to-find-apportionments"
           >FAQ section on searching apportionments</a
         > for further guidance.
-      </dd>
+      </li>
     {:else}
-      <dd>Keywords must consist of full term(s), not abbreviations or acronyms.</dd>
+      <li>Keywords must consist of full term(s), not abbreviations or acronyms.</li>
     {/if}
-  </dl>
+  </ul>
 </svelte.fragment>
-
-<style>
-  dl {
-    color: var(--color-text-muted);
-  }
-  dh {
-    font-weight: var(--font-copy-weight-bold);
-  }
-</style>
