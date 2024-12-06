2189b2e10d4413befee4c210d5ca10ceb606e378
Alan Palazzolo <190733+zzolo@users.noreply.github.com>
2024-12-06 13:59:30 -0500

    Using search results across other file listings.

--- src/lib/formatters.ts
+++ src/lib/formatters.ts
@@ -122,7 +122,7 @@ export const formatTafsFormattedId = (tafsRecord: tafsSelect): string => {
  */
 export const highlight = function (text?: string | null, terms?: string[], trim?: number): string {
   terms = terms ? terms : [];
-  terms = filter(terms.map((t) => t.trim()));
+  terms = filter(terms.map((t) => t?.trim())).filter(Boolean);
 
   if (!terms || terms.length < 1 || !text) {
     return text || '';
