76abc4243409b747a3a877220a8cc9d4b55ac7a8
Alan Palazzolo <190733+zzolo@users.noreply.github.com>
2024-12-16 11:20:44 -0500

    Trying to fix migration issues where extension is not available.

--- db/migrations/0002_peaceful_roland_deschain.sql
+++ db/migrations/0002_peaceful_roland_deschain.sql
@@ -1,3 +1,4 @@
+CREATE EXTENSION pg_trgm;--> statement-breakpoint
 ALTER TABLE "files" ADD COLUMN "source_text" text;--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "file_source_text_index" ON "files" USING gin (source_text gin_trgm_ops);--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "footnote_text_index" ON "footnotes" USING gin (footnote_text gin_trgm_ops);
