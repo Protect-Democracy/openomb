5d7e4d323be5efb0b271cc4cc46a633c120bbbe3
Megan Lyle <megan.lyle@voteshield.us>
2025-03-13 16:44:24 -0500

    add secure cookie setting back in (needed)

--- src/auth.ts
+++ src/auth.ts
@@ -3,9 +3,12 @@ import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { db } from '$db/connection';
 import { sendVerificationRequest } from '$lib/server/auth-notify';
 import { users, accounts, sessions, verificationTokens } from '$db/schema';
+import env from '$lib/environment';
 
 const { handle, signIn, signOut } = SvelteKitAuth({
   trustHost: true,
+  // Secure cookie https detection may be bugged, so set based on environment
+  useSecureCookies: env.environment === 'development' ? false : true,
   adapter: DrizzleAdapter(db, {
     usersTable: users,
     accountsTable: accounts,
