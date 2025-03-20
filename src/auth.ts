51dc1f91898fdf1a6c879c67af2f5bb671264a56
Megan Lyle <megan.lyle@voteshield.us>
2025-03-20 14:10:19 -0500

    fix secure email links and redirect urls (AUTH_URL env variable)

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,6 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import type { Handle } from '@sveltejs/kit';
 import { db } from '$db/connection';
 import { sendVerificationRequest } from '$lib/server/auth-notify';
 import { users, accounts, sessions, verificationTokens } from '$db/schema';
@@ -30,8 +31,18 @@ const { handle, signIn, signOut } = SvelteKitAuth({
   }
 });
 
-// Replace the auth handle function so that our session is an
-// unwrapped promise and actually triggers our load functions
-const authHandle = handle;
+// Replace the auth handle function so that we can handle
+//  HEAD requests and prevent them using up access codes
+//  https://next-auth.js.org/tutorials/avoid-corporate-link-checking-email-provider
+const authHandle: Handle = async (props) => {
+  // If it is an email client HEAD request, bypass authentication handle
+  if (
+    props.event.request.method === 'HEAD' &&
+    Boolean(props.event.url.pathname.match(/^\/(auth|subscribe).*/))
+  ) {
+    return await props.resolve(props.event);
+  }
+  return await handle(props);
+};
 
 export { authHandle, signIn, signOut };
