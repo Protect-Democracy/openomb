e05e469eac7be12c521b641b31fef60c4d6db60e
Alan Palazzolo <190733+zzolo@users.noreply.github.com>
2026-03-07 16:26:10 -0500

    More basic testing for static-ish pages.

--- tests/integration/basic-site.integration-test.ts
+++ tests/integration/basic-site.integration-test.ts
@@ -12,7 +12,21 @@ test('index page has expected items', async ({ page }) => {
   await expect(page.locator('.recently-approved-files ul li')).toHaveCount(10);
 });
 
-test('about page has expected h1', async ({ page }) => {
-  await page.goto('/about');
-  await expect(page.locator('main').getByRole('heading', { name: 'About' })).toBeVisible();
+test('static pages', async ({ page }) => {
+  const pages = [
+    { path: '/about', heading: 'About' },
+    { path: '/explore', heading: 'Explore' },
+    { path: '/folders', heading: 'Agencies' },
+    { path: '/faq', heading: 'Frequently' },
+    { path: '/developers', heading: 'Developers' },
+    { path: '/terms', heading: 'OpenOMB Terms and Conditions' },
+    { path: '/privacy-policy', heading: 'OpenOMB Privacy Policy' }
+  ];
+
+  for (const { path, heading } of pages) {
+    await page.goto(path);
+    await expect(
+      page.locator('main').getByRole('heading', { name: heading, exact: false })
+    ).toBeVisible();
+  }
 });
