1376faccf15965519e5a191e1c71a16b07d696e4
Alan Palazzolo <190733+zzolo@users.noreply.github.com>
2026-05-08 19:22:35 -0400

    Making login form able to handle client side login.

--- src/components/subscriptions/LogIn.svelte
+++ src/components/subscriptions/LogIn.svelte
@@ -5,22 +5,55 @@
   // Props
   export let callbackUrl: string;
   export let headingClass: string = 'h1';
+
+  // State
+  let userEmail: string | null = null;
+  let emailSent = false;
+
+  // Handle successful login
+  function onLoginSuccess(email: string) {
+    userEmail = email;
+    emailSent = true;
+  }
+
+  // Handle login reset
+  function resetLogin() {
+    userEmail = null;
+    emailSent = false;
+  }
 </script>
 
-<h2 class={headingClass}>Log in or create an account</h2>
+<div>
+  {#if emailSent}
+    <h2 class={headingClass}>Check your email</h2>
 
-<p>
-  Enter your email address to log in or create a new subscription account. You will be sent a “magic
-  link” with your account email to log in, enabling you to subscribe to updates for various feeds.
-</p>
+    <p class="callout-container-large">
+      A sign-in link has been sent to {#if userEmail}<br /><strong>{userEmail}</strong>{:else}your
+        email address{/if}.
+    </p>
 
-<LogInForm {callbackUrl} />
+    <p>
+      Entered the wrong email?
+      <button class="like-link compact" on:click|preventDefault={resetLogin}>Start over</button>
+    </p>
+  {:else}
+    <h2 class={headingClass}>Log in or create an account</h2>
 
-<small>
-  By logging in and subscribing to email updates you agree to the OpenOMB's <a
-    href={resolve('/privacy-policy')}>privacy policy</a
-  >.
-</small>
+    <p>
+      Enter your email address to log in or create a new subscription account. You will be sent a
+      “magic link” with your account email to log in, enabling you to subscribe to updates for
+      various feeds.
+    </p>
+
+    <LogInForm {callbackUrl} defaultLoginMessage={false} onLogin={onLoginSuccess} />
+  {/if}
+
+  <small>
+    By logging in and subscribing to email updates you agree to the OpenOMB's <a
+      href={resolve('/privacy-policy')}>privacy policy</a
+    >.
+  </small>
+</div>
 
 <style>
   h2 {
@@ -42,5 +75,9 @@
     text-align: center;
     margin-top: calc(var(--spacing) * 3);
     color: var(--color-text-muted);
+  }
+
+  .callout-container-large {
+    margin-bottom: var(--spacing);
   }
 </style>
--- src/components/subscriptions/LogInForm.svelte
+++ src/components/subscriptions/LogInForm.svelte
@@ -7,34 +7,120 @@
   Params
     - callbackUrl: Url to redirect the user to once they've signed in
     - action: Text to use on login button (defaults to Authenticate)
+    - defaultLoginMessage: Whether to show the default login message after
+      successful login (defaults to true).  Useful to turn off if you want to handle showing
+      a message on successful login within the parent component.
+  Events
+    - onLogin: Triggered when login is successful and passes the email address used to login.
+    - onLoginError: Triggered when login is unsuccessful and passes the email address used to login.
   Slots
     None
 -->
 <script lang="ts">
+  import { signIn } from '@auth/sveltekit/client';
+  import SpinnerIcon from '$components/icons/Spinner.svelte';
+
+  // Constatns
+  const defaultErrorMessage = 'Something went wrong. Please try again.';
+
   // Props
-  export let callbackUrl;
+  export let callbackUrl: string;
   export let action = 'Send link';
+  export let actionLoading = 'Sending…';
+  export let defaultLoginMessage: boolean = true;
+  // Event prop that gets triggers when login is successful and passes the email
+  // address used to login.
+  export let onLogin: ((email: string) => void) | null = null;
+  // Event prop that gets triggers when login is unsuccessful and passes the email
+  // address used to login.
+  export let onLoginError: ((email: string) => void) | null = null;
+
+  // State
+  let email = '';
+  let submitted = false;
+  let loading = false;
+  let errorMessage: string | null = null;
+
+  // Client side login function
+  async function handleSubmit() {
+    loading = true;
+    errorMessage = null;
+
+    try {
+      const result = await signIn('http-email', { email, callbackUrl, redirect: false });
+      // If we wanted to redirect, result.url should be in response
+
+      if (result?.ok && !result.error) {
+        submitted = true;
+        if (onLogin) {
+          onLogin(email);
+        }
+      } else if (result.error) {
+        // Unsure if error message is user friendly.  If a non-email is sent, the error
+        // message is just "Configuration"
+        errorMessage = defaultErrorMessage;
+        if (onLoginError) {
+          onLoginError(email);
+        }
+      } else {
+        errorMessage = defaultErrorMessage;
+        if (onLoginError) {
+          onLoginError(email);
+        }
+      }
+    } catch {
+      errorMessage = defaultErrorMessage;
+      if (onLoginError) {
+        onLoginError(email);
+      }
+    } finally {
+      loading = false;
+    }
+  }
 </script>
 
-<form class="login-form" action="/auth/signin/http-email" method="POST">
-  <input type="hidden" name="csrfToken" />
-  <input type="hidden" name="providerId" value="email" />
-  <input type="hidden" name="callbackUrl" value={callbackUrl} />
+{#if defaultLoginMessage && submitted}
+  <p>
+    An email has been sent to <strong>{email}</strong>. Entered the wrong email?
+    <button class="like-link compact" on:click={() => (submitted = false)}>Start over</button>
+  </p>
+{:else}
+  <form
+    class="login-form"
+    action="/auth/signin/http-email"
+    method="POST"
+    on:submit|preventDefault={handleSubmit}
+  >
+    <input type="hidden" name="csrfToken" />
+    <input type="hidden" name="providerId" value="email" />
+    <input type="hidden" name="callbackUrl" value={callbackUrl} />
 
-  <div class="login-input">
-    <label for="input-email-for-http-email-provider" class="sr-only">Email</label>
+    <div class="login-input">
+      <label for="input-email-for-http-email-provider" class="sr-only">Email</label>
 
-    <input
-      id="input-email-for-http-email-provider"
-      type="email"
-      name="email"
-      placeholder="email@example.com"
-      required
-    />
-  </div>
+      <input
+        id="input-email-for-http-email-provider"
+        name="email"
+        type="email"
+        placeholder="email@example.com"
+        bind:value={email}
+        required
+      />
+    </div>
 
-  <button type="submit">{action}</button>
-</form>
+    <button type="submit" disabled={loading}>
+      {#if loading}
+        <span class="button-icon"><SpinnerIcon /></span> {actionLoading}
+      {:else}
+        {action}
+      {/if}
+    </button>
+  </form>
+
+  {#if errorMessage}
+    <p class="error">{errorMessage}</p>
+  {/if}
+{/if}
 
 <style>
   .login-form {
@@ -87,5 +173,14 @@
         width: 100%;
       }
     }
+  }
+
+  .button-icon {
+    margin-left: calc(var(--spacing) * -1);
+  }
+
+  .error {
+    color: var(--color-error);
+    font-size: var(--font-size-small);
   }
 </style>
